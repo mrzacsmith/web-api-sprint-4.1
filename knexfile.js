10cc10afcf1353df7863e88ffa87042f9ab24252
Luis Hernandez <luishrd@gmail.com>
2020-07-30 10:31:27 -0700

    renames data folder to database

--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,39 @@
 module.exports = {
-    development: {
-        client: "sqlite3",
-        useNullAsDefault: true,
-        connection: {
-            filename: "./data/lambda.db3",
-        },
-        pool: {
-            afterCreate: (conn, done) => {
-                conn.run("PRAGMA foreign_keys = ON", done);
-            },
-        },
-        migrations: {
-            directory: "./data/migrations",
-        },
-        seeds: {
-            directory: "./data/seeds",
-        },
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/gtd.db3',
     },
-    production: {
-        client: "sqlite3",
-        useNullAsDefault: true,
-        connection: {
-            filename: "./data/lambda.db3",
-        },
-        pool: {
-            afterCreate: (conn, done) => {
-                conn.run("PRAGMA foreign_keys = ON", done);
-            },
-        },
-        migrations: {
-            directory: "./data/migrations",
-        },
-        seeds: {
-            directory: "./data/seeds",
-        },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/gtd.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
