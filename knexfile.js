150dd988765656161e04dae145e9d3ea4f5c7790
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-13 20:59:15 +0100

    add tests

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,24 @@ module.exports = {
       directory: './database/seeds',
     },
   },
-
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/testgtd.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
   production: {
     client: 'sqlite3',
     useNullAsDefault: true,
